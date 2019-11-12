e2ab50c3f1af805081529b66c3ca3a669418d59e
MMuta52 <matthew.mutammara@gmail.com>
2019-11-12 00:28:07 -0600

    Restructured to correctly run for case without precomputed adjacency matrix

--- SemanticNetwork.py
+++ SemanticNetwork.py
@@ -7,30 +7,7 @@ import pandas as pd
 import networkx as nx
 from newspaper import Article
 
-class SemanticNetwork:
-    def __init__(self, adj_path, sectors_path):
-        # If given path to CSV representation of adjacency matrix, just load it in
-        if os.path.isfile(adj_path):
-            adj = pd.read_csv(adj_path, index_col=0)
-        # If given path to folder containing CSV's of URL's, create adjacency matrix from these files
-        elif os.path.isdir(adj_path):
-            companies, urls = load_urls('adj_path')
-            adj = count_mentions(companies, urls)
-            # Save to CSV for future use
-            adj.to_csv(adj_path + '.csv')
-        else:
-            raise ValueError('Input should be path to adjacency matrix CSV or folder of CSVs of URLs')
-        
-        self.adj = adj
-        self.graph = nx.from_pandas_adjacency(adj)
-
-        # Add sector as attribute
-        sector_df = pd.read_csv(sectors_path)
-        sector_map = {row['CommonName']:row['Sector'] for __, row in sector_df.iterrows()}
-        # sector_map = sector_df['CommonName'].map(sector_df['Sector'])
-        nx.set_node_attributes(self.graph, sector_map, name='sector')        
-
-    def load_urls(urls_path):
+def load_urls(urls_path):
         """
         Use this if given path to folder of URL's
         Input: Path to folder containing CSV files that contain URL's
@@ -54,44 +31,66 @@ class SemanticNetwork:
 
         return companies, urls
 
+def read_url(url):
+    """
+    Read text from url using Newspaper API
+    Returns article content as one continuous string
+    """
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article.text
 
-    def read_url(url):
-        """
-        Read text from url using Newspaper API
-        Returns article content as one continuous string
-        """
-        article = Article(url)
-        article.download()
-        article.parse()
-        return article.text
+def count_mentions(companies, urls):
+    """
+    Create adjacency matrix of companies given set of URL's to articles
+    """
+    zero = np.zeros((len(companies),len(companies)))
+    counts = pd.DataFrame(zero,index=companies, columns=companies)
 
-    def count_mentions(companies, urls):
-        """
-        Create adjacency matrix of companies given set of URL's to articles
-        """
-        zero = np.zeros((len(companies),len(companies)))
-        counts = pd.DataFrame(zero,index=companies, columns=companies)
+    count = 1
+    print('Reading URLs...')
+    for url in urls:
+        if count % 100 == 0:
+            print('Reading URL %d of %d' % (count, len(urls)))
 
-        count = 1
-        print('Reading URLs...')
-        for url in urls:
-            if count % 100 == 0:
-                print('Reading URL %d of %d' % (count, len(urls)))
+        try:
+            article = read_url(url)
+        except:
+            article = ''
 
-            try:
-                article = read_url(url).lower()
-            except:
-                article = ''
+        # Check which companies are in the article
+        in_article = [comp for comp in companies if comp in article]
+        for comp1 in in_article:
+            for comp2 in in_article:
+                if comp1 != comp2:
+                    counts[comp1][comp2] += 1
 
-            # Check which companies are in the article
-            in_article = [comp for comp in companies if comp.lower() in article]
-            for comp1 in in_article:
-                for comp2 in in_article:
-                    if comp1 != comp2:
-                        counts[comp1][comp2] += 1
-
-            count += 1
+        count += 1
 
 
-        return counts
+    return counts
+
+class SemanticNetwork:
+    def __init__(self, adj_path, sectors_path):
+        # If given path to CSV representation of adjacency matrix, just load it in
+        if os.path.isfile(adj_path):
+            adj = pd.read_csv(adj_path, index_col=0)
+        # If given path to folder containing CSV's of URL's, create adjacency matrix from these files
+        elif os.path.isdir(adj_path):
+            companies, urls = load_urls(adj_path)
+            adj = count_mentions(companies, urls)
+            # Save to CSV for future use
+            adj.to_csv(adj_path + '.csv')
+        else:
+            raise ValueError('Input should be path to adjacency matrix CSV or folder of CSVs of URLs')
+        
+        self.adj = adj
+        self.graph = nx.from_pandas_adjacency(adj)
+
+        # Add sector as attribute
+        sector_df = pd.read_csv(sectors_path)
+        sector_map = {row['CommonName']:row['Sector'] for __, row in sector_df.iterrows()}
+        nx.set_node_attributes(self.graph, sector_map, name='sector')        
+
 
